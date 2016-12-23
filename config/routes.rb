cb1eacb23a9a5750627be09752b9d3d23392b06b
Raghu Betina <raghu@firstdraft.com>
2016-12-23 11:30:17 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "businesses#index"
   # Routes for the Ownership resource:
   # CREATE
