3687b818aa567fc898db457476ca2d21ab5fdaf7
Raghu Betina <raghu@firstdraft.com>
2016-12-23 11:30:20 +0000

    Whitelist columns in Business for ActiveAdmin

--- app/admin/business.rb
+++ app/admin/business.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Business do
 
+ permit_params :address, :name, :description, :website, :cover_photo, :ownership_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
